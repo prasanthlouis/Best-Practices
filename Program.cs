5d344bfa7373fe49ce079480b441c02923b0eef3
Prasanth Louis <prasanthlouis21@gmail.com>
2016-09-27 00:02:18 -0400

    Next Commit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,51 @@ namespace Check
     {
         static void Main(string[] args)
         {
-            bool? check;
-            check = null;
-            Console.WriteLine(check);
+            //reference type
+            string name = null;
+
+            //value type made nullable
+            int? number = null;
+
+            //value type by default is null
+            bool? isTrue;
+
+            ProgramX program = null;
+
+            //Evaluates to null
+            Console.WriteLine(program?.Name);
+
+            program = new ProgramX();
+
+            //Evaluates to null
+            Console.WriteLine(program?.Name);
+
+            program = new ProgramX() { Name = "Rumpley" };
+
+            //Evaluates to Rumpley
+
+            Console.WriteLine(program?.Name);
+
+            program = new ProgramX();
+
+            //Evaluates to No Name
+            Console.Write(program.Name ?? "No Name\n");
+
+            //ProgramX programBreak = null;
+
+            //Run Time Error
+            //Console.WriteLine(programBreak.Name ?? "No Name");
+
+            //Evaluates to no name
+            Console.WriteLine(program.Name != null ? program.Name : "No Name\n");
+
             Console.ReadLine();
         }
+
+    }
+
+    class ProgramX
+    {
+        public string Name { get; set; }
     }
 }
