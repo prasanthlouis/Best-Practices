314d3f1d87950a5e2f25cb7a3ed25c33d7b3ad5d
Prasanth Louis <prasanthlouis21@gmail.com>
2016-09-26 22:41:54 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? check;
+            check = null;
+            Console.WriteLine(check);
+            Console.ReadLine();
+        }
+    }
+}
